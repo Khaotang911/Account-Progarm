38fecf08e0aaee67dba518243e48568ec37d3e3a
Khaotang911 <102736845+Khaotang911@users.noreply.github.com>
2022-04-06 21:20:39 +0700

    Add files via upload

--- Account.py
+++ Account.py
@@ -10,17 +10,125 @@ import pandas as pd
 import tabulate
 import numpy as np
 import os
+import matplotlib.pyplot as plt
+
 
 class openAccount:
+
+
+    # ฟังก์ชั่นหลัก
+    def choice(self, choose, file_name):
+        if choose == 1:
+            print('พิมพ์วันที่และจำนวนเงินที่ต้องการ\n'
+                  'โดยพิมพ์รูปแบบนี้ ตัวอย่าง\n'
+                  'วันที่: 2565/3/25 คือ วันที่ 25 เดือน 3 ปี 2565\n'
+                  'เงิน: +200 คือรายรับ 200\n'
+                  '    -300 คือรายจ่าย 300\n'
+                  'หากจะยกเลิกให้พิมพ์ pass ในช่อง วันที่ต้องการบันทึก')
+            day = input('วันที่ต้องการบันทึก :')
+            try:
+                if day != 'pass':
+                    money = int(input('จำนวนเงิน :'))
+                    description = input('รายละเอียด :')
+                else:
+                    print(" ")
+                    pass
+                self.incomeOutcome(day, money, description, file_name)
+            except:
+                pass
+        elif choose == 2:
+            print('ต้องการตรวจสอบยอดเป็นรายวันพิมพ์ 1\n'
+                  'ต้องการตรวจสอบยอดเป็นรายเดือนพิมพ์ 2\n'
+                  'ต้องการตรวจสอบยอดทั้งหมดพิมพ์ 3\n'
+                  'หากต้องการกลับไปยังหน้าเดิมก่อนหน้าพิมพ์ 4')
+            choose2 = int(input(':'))
+            if choose2 == 1:
+                print('พิมพ์วันที่ที่ท่านต้องการตรวจสอบยอด \n'
+                      'รูปแบบดังนี้ ตัวอย่าง 2565/3/25')
+                dayInput = input(':')
+                self.show_file_month_day(dayInput, file_name)
+                self.plotDay(dayInput, file_name)
+            elif choose2 == 2:
+                print('พิมพ์เดือนที่ท่านต้องการตรวจสอบยอด \n'
+                      'รูปแบบดังนี้ ตัวอย่าง 2565/3 คือ เดือน 3 ปี 2565')
+                monthInput = input(':')
+                self.show_file_month_day(monthInput, file_name)
+                self.plotMonth(monthInput, file_name)
+            elif choose2 == 3:
+                self.showAll(file_name)
+            elif choose2 == 4:
+                pass
+            else:
+                print('สิ่งที่ผู้ใช้ป้อนไม่อยู่ในระบบ!!! กรุณาป้อนข้อมูลตามที่กำหนด\n')
+        else:
+            pass
+
+    # ฟังก์ชั่นย่อย
+    def show_file_month_day(self, day_month, file_name):
+        try:
+            df = pd.read_csv(file_name)
+            self.showTotal(df)
+            df = df.drop(columns='Money')
+            final_df = df.loc[df['Day'].str.contains(day_month, na=False)]
+            print(tabulate.tabulate(final_df, headers='keys', showindex=False), "\n")
+        except:
+            print("ไม่สามารถอ่านไฟล์ได้ อาจเป็นไฟล์ที่ไม่มีข้อมูลหรือข้อมูลเสียหาย\n")
+
+    def showAll(self, file_name):
+        try:
+            df = pd.read_csv(file_name)
+            self.showTotal(df)
+            df = df.drop(columns='Money')
+            print(tabulate.tabulate(df, headers='keys', showindex=False), "\n")
+        except:
+            print("ไม่สามารถอ่านไฟล์ได้ อาจเป็นไฟล์ที่ไม่มีข้อมูลหรือข้อมูลเสียหาย\n")
+
+    def incomeOutcome(self, Day, Money, Description, Filename):
+
+        df = pd.read_csv(Filename)
+        col = ['No.', 'Day', 'Description', 'Income', 'Expense', 'Total', 'Money']
+        if Money > 0:
+            Income = Money
+            Expense = ' '
+        elif Money < 0:
+            Income = ' '
+            Expense = Money * (-1)
+        Total = 0
+
+        newData = np.array([(len(df) + 1, Day, Description, Income, Expense, Total, Money)])
+        newDF = pd.DataFrame(data=newData, columns=col)
+        df = pd.concat([df, newDF])
+        df = df.sort_values(['Day'])
+
+        df['No.'] = np.arange(1, len(df) + 1)
+
+        showDF = newDF.drop(columns=["Total", "Money"])
+        print(showDF)
+        print("\nผู้ใช้แน่ใจหรือไม่ที่จะบันทึกรายการนี้? พิมพ์ Y หากแน่ใจ")
+        make_sure = input(":")
+        if make_sure.upper() == 'Y':
+            df.to_csv(Filename, index=False)
+        else:
+            pass
+
+    def showTotal(self, data):
+        Total = 0
+        for i in range(len(data)):
+            Total = Total + data.iloc[i, -1]
+            data.iloc[i, -2] = Total
 
     def checkFolder(self):
         try:
             if not os.path.exists('D:\AccountFile'):
-                os.makedirs('D:\AccountFile')
+                print("ทางโปรแกรมพบว่าท่านไม่มีไฟล์ที่ใช้จัดเก็บข้อมูลสำหรับโปรแกรม\n"
+                      "จึงขออนุญาติสร้างโฟลเดอร์เก็บข้อมูล\n"
+                      "ท่านต้องการสร้างโฟล์เดอร์ไว้ที่ไดรฟ์ไหนระหว่าง C หรือ D")
+                drive_name = input(":")
+                os.makedirs(drive_name.upper()+':\AccountFile')
         except OSError:
             pass
 
-    def WriteFile(self):
+    def writeFile(self):
         name = input('พิมพ์ชื่อไฟล์ที่ต้องการจะตั้ง :')
         file_name = 'D:\AccountFile/' + name + '.csv'
 
@@ -33,7 +141,6 @@ class openAccount:
 
         return file_name
 
-
     def selectingFile(self):
         print("รายชื่อไฟล์ที่มีอยู่:")
         for i in os.listdir(r'D:\AccountFile'):
@@ -44,104 +151,73 @@ class openAccount:
         if fname in os.listdir(r'D:\AccountFile'):
             file_name = 'D:\AccountFile/' + name + '.csv'
         else:
-            print("ไม่มีไฟล์อยู่ในระบบ กรุณาสร้างไฟล์เพื่อบันทึก")
+            print("ไม่มีไฟล์ที่ท่านระบุอยู่ในระบบ กรุณาสร้างไฟล์เพื่อบันทึก")
         return file_name
 
-
-
-    # ฟังก์ชั่นหลัก
-    def Choice(self, choose, Filename):
-        if choose == 1 :
-            print('พิมพ์วันที่และจำนวนเงินที่ต้องการ\n'
-              'โดยพิมพ์รูปแบบนี้ ตัวอย่าง\n'
-              'วันที่: 2565/3/25 คือ วันที่ 25 เดือน 3 ปี 2565\n'
-              'เงิน: +200 คือรายรับ 200\n'
-              '    -300 คือรายจ่าย 300\n'
-              'หากจะยกเลิกให้พิมพ์ pass ในช่อง วันที่ต้องการบันทึก')
-            day = input('วันที่ต้องการบันทึก :')
-            money = int(input('จำนวนเงิน :'))
-            description = input('รายละเอียด :')
-            self.IncomeOutcome(day, money, description, Filename)
-        elif choose == 2 :
-            print('ต้องการตรวจสอบยอดเป็นรายวันพิมพ์ 1\n'
-              'ต้องการตรวจสอบยอดเป็นรายเดือนพิมพ์ 2\n'
-              'ต้องการตรวจสอบยอดทั้งหมดพิมพ์ 3\n'
-              'หากต้องการกลับไปยังหน้าเดิมก่อนหน้าพิมพ์ 4')
-            choose2 =int(input(':'))
-            if choose2 == 1:
-                print('พิมพ์วันที่ที่ท่านต้องการตรวจสอบยอด \n'
-                  'รูปแบบดังนี้ ตัวอย่าง 2565/3/25')
-                dayInput = input(':')
-                self.showAccountDay(dayInput, Filename)
-            elif choose2 == 2:
-                print('พิมพ์เดือนที่ท่านต้องการตรวจสอบยอด \n'
-                  'รูปแบบดังนี้ ตัวอย่าง 2565/3 คือ เดือน 3 ปี 2565')
-                monthInput = input(':')
-                self.showAccountMonth(monthInput, Filename)
-            elif choose2 == 3:
-                self.showAll(Filename)
-            elif choose2 == 4:
-                pass
-            else:
-                print('สิ่งที่ผู้ใช้ป้อนไม่อยู่ในระบบ!!! กรุณาป้อนข้อมูลตามที่กำหนด\n')
-        else:
-            print('ERROR')
-
-    # ฟังก์ชั่นย่อย
-    def IncomeOutcome(self,Day, Money, Description, Filename):
-
-        df = pd.read_csv(Filename)
-        col= ['No.', 'Day', 'Description', 'Income', 'Expense', 'Total', 'Money']
-        if Money > 0 :
-            Income = Money
-            Expense = ' '
-        elif Money < 0 :
-            Income = ' '
-            Expense = Money*(-1)
-        Total = 0
-
-        newData = np.array([(len(df)+1, Day, Description, Income, Expense, Total, Money)])
-        newDF = pd.DataFrame(data=newData, columns=col)
-        df = pd.concat([df, newDF])
-        df = df.sort_values(['Day'])
-
-
-        df['No.'] = np.arange(1, len(df)+1)
-
-        df.to_csv(Filename, index=False)
-
-    def showAccountDay(self, day, Filename):
-        df = pd.read_csv(Filename)
+    def plotDay(self, day, file_name):
+        df = pd.read_csv(file_name)
+        df = df.loc[df['Day'].str.contains(day, na=False)]
         self.showTotal(df)
-        df.drop(columns='Money')
-        Daydf = df.loc[df['Day'] == day]
-        print(tabulate.tabulate(Daydf, headers='keys', showindex=False),"\n")
+        no = []
+        total = []
+        for i in range(len(df)):
+            newdf = df.loc[df['Day'].str.contains(day, na=False)]
+            no.append(i + 1)
+            total.append(list(newdf['Total'])[i])
 
+        fig = plt.figure(figsize=(15, 5))
+        ax = fig.add_subplot(111)
+        ax.plot(no, total, marker='*')
+        plt.title("Income-expense of the account in the day " + day)
+        plt.xlabel("Date of receipt of income - expense (Spending Cycle)")
+        plt.ylabel("Amount (Baht)")
+        plt.show()
 
-    def showAccountMonth(self, month, Filename):
-        df = pd.read_csv(Filename)
+    def plotMonth(self, month, file_name):
+        df = pd.read_csv(file_name)
+        df = df.loc[df['Day'].str.contains(month + '/', na=False)]
+
+        day = []
+        total = []
         self.showTotal(df)
-        df.drop(columns='Money')
-        Monthdf = df.loc[df['Day'].str.contains(month, na=False)]
-        print(tabulate.tabulate(Monthdf, headers='keys', showindex=False),"\n")
+        for i in range(len(df)):
+            dd = df['Day'].str.split('/')
+            day.append(int(dd[i][2]))
+        day = set(day)
+        day = list(day)
+        for i in day:
+            newdf = df.loc[df['Day'].str.contains(month + '/' + str(i), na=False)]
+            total.append(newdf['Total'].sum())
 
-    def showAll(self, Filename):
-        df = pd.read_csv(Filename)
-        self.showTotal(df)
-        df.drop(columns='Money')
-        print(tabulate.tabulate(df, headers='keys', showindex=False),"\n")
+        fig = plt.figure(figsize=(15, 5))
+        ax = fig.add_subplot(111)
+        ax.plot(day, total, marker='*')
+        plt.ylim(min(total), max(total))
+        plt.xlim(1, 31)
+        plt.title("Income-expense of the account in the month " + month)
+        plt.xlabel("Date of receipt of income - expense (Date)")
+        plt.ylabel("Amount (Baht)")
+        plt.show()
 
-    def showTotal(self, data):
-        Total = 0
-        for i in range(len(data)):
-            Total = Total + data.iloc[i, -1]
-            data.iloc[i, -2] = Total
+    def strat_menu(self):
+        print('    โปรแกรมบันทึกรายรับรายจ่าย\n'
+              '\n'
+              '          จัดทำโดย               \n'
+              'นายธณรัฐ เพียรชัยภูมิ 643040011-1\n'
+              'นายพีระณัฐ ศรีสรรพ์ 643040249-8\n'
+              'นางสาวภูริชญดา สนิทชน 643040251-1\n')
 
-    #ฟังก์ชั่นการทำงานของโปรแกรม
+        try:
+            input("กดที่ปุ่ม <Enter> เพื่อไปใช้งานโปรแกรม")
+        except SyntaxError:
+            pass
+
+    # ฟังก์ชั่นการทำงานของโปรแกรม
     def __init__(self):
+        self.strat_menu()
         self.checkFolder()
         while True:
-            print("ผู้ใช้ต้องการดำเนินการกับไฟล์ที่มีหรือต้องการสร้างไฟล์ใหม่\n"
+            print("ผู้ใช้ต้องการดำเนินการกับไฟล์ที่มีหรือต้องการสร้างไฟล์ใหม่?\n"
                   "ต้องการดำเนินการกับไฟล์ที่มีอยู่พิมพ์ 1\n"
                   "ต้องการสร้างไฟล์ใหม่ในการบันทึกพิมพ์ 2\n"
                   "ปิดการทำงานของโปรแกรมพิมพ์ 3")
@@ -151,7 +227,7 @@ class openAccount:
                 if file_select == 1:
                     file_name = self.selectingFile()
                 elif file_select == 2:
-                    file_name = self.WriteFile()
+                    file_name = self.writeFile()
                 elif file_select == 3:
                     break
                 else:
@@ -160,21 +236,20 @@ class openAccount:
                 while True:
 
                     print('พิมพ์ 1 เพื่อบันทึกรายรับ รายจ่าย\n'
-                'พิมพ์ 2 เพื่อเรีกดูบัญชี\n'
-                'พิมพ์ 3 เพื่อกลับไปยังหน้าจัดการไฟล์\n'
-                  'หมายเหตุ: หากมีการกรอกข้อมูลผิดพลาด โปรแกรมจะกลับมายังหน้าต่างนี้ทันที')
+                          'พิมพ์ 2 เพื่อเรีกดูบัญชี\n'
+                          'พิมพ์ 3 เพื่อกลับไปยังหน้าจัดการไฟล์\n'
+                          'หมายเหตุ: หากมีการกรอกข้อมูลผิดพลาด โปรแกรมจะกลับมายังหน้าต่างนี้ทันที\n')
                     Finput = input(':')
 
                     try:
                         if int(Finput) == 3:
                             break
                         elif int(Finput) in [1, 2]:
-                            self.Choice(int(Finput), file_name)
+                            self.choice(int(Finput), file_name)
                         else:
                             print('สิ่งที่ผู้ใช้ป้อนไม่อยู่ในระบบ!!! กรุณาป้อนข้อมูลตามที่กำหนด\n')
                     except:
                         print('สิ่งที่ผู้ใช้ป้อนไม่อยู่ในระบบ!!! กรุณาป้อนข้อมูลตามที่กำหนด\n')
             except:
                 print('สิ่งที่ผู้ใช้ป้อนไม่อยู่ในระบบ!!! กรุณาป้อนข้อมูลตามที่กำหนด\n')
-
 openAccount()
